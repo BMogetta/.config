58291e8e35eb53eebcf27321790036d6536e9d63
Bruno Mogetta <100445144+BMogetta@users.noreply.github.com>
2022-12-29 10:56:50 -0300

    Update path.sh

--- path.sh
+++ path.sh
@@ -8,6 +8,14 @@ export PATH=/usr/local/bin:${PATH}:/sbin:/usr/local/sbin
 
 # add go path
 export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
+export PATH=$PATH:/usr/local/go/bin
+export PATH="/home/bruno/go/bin:$PATH"
 
 # add rust path
 export PATH="$HOME/.cargo/bin:$PATH"
+
+# deno
+export PATH=/home/bruno/.deno/bin:$PATH
+
+# pnpm
+export PATH="/home/bruno/.local/share/pnpm:$PATH"
